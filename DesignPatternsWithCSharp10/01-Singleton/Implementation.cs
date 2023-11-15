3b27266848c9b846d4b6dc51caaa1883fdf8041d
Edward Mahama <edwardmahama@microsoft.com>
2023-11-15 16:21:14 -0500

    add singletion

---
+++ DesignPatternsWithCSharp10/01-Singleton/Implementation.cs
@@ -0,0 +1,44 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    /// Singleton Class
+    /// </summary>
+    public class Logger
+    {
+        /// <summary>
+        /// Backing field for the Instance property
+        /// </summary>
+        // private static Logger? _instance;
+
+        private static Lazy<Logger?> _lazyInstance = new Lazy<Logger?>(() => new Logger());
+
+        /// <summary>
+        ///  protected or private constructor 
+        /// </summary>
+        protected Logger()
+        {}
+
+        /// <summary>
+        /// Public property for accessing the singleton instance
+        /// </summary>
+        public static Logger? Instance
+        {
+            get 
+            { 
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+
+                return _lazyInstance.Value;
+            }
+        }
+
+        public void Log(string message) 
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
---
+++ DesignPatternsWithCSharp10/01-Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Singleton;
+
+Console.Title = "Singletion Examplet";
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2)
+{
+    Console.WriteLine($"{nameof(instance1)} and {nameof(instance2)} are the same");
+}
+
+instance1?.Log($"Logging from {nameof(instance1)}");
+instance2?.Log($"Logging from {nameof(instance2)}");
+Console.WriteLine($"Logging from {nameof(Logger.Instance)}");
